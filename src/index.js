e05955b59472ed95e93a21b76fbc4ffff939251f
Karen Habuzian <karen.gabuzyan@gmail.com>
2022-06-28 23:34:05 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    titleChanged,
+    taskDeleted, 
+    completeTask,
+    createTask,
+    loadTasks,
+    getLTasksLoadingStatus,
+    getTasks
+} from "./store/task";
+import configureStore from './store/store';
+import { Provider } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { getError } from './store/errors';
+
+const store = configureStore();
+const App = () => {
+    const state = useSelector(getTasks());
+    const isLoading = useSelector(getLTasksLoadingStatus());
+    const error = useSelector(getError());
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadTasks());
+    }, []);
+    const changeTitle = (taskId) => {
+        dispatch(titleChanged(taskId));
+    };
+    const deleteTask = (taskId) => {
+        dispatch(taskDeleted(taskId));
+    };
+    const createNewTask = () => {
+        dispatch(createTask({
+            title: "some title",
+            completed: "false"
+        }))
+    };
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <p>{error}</p>
+    }
+    return (
+        <>
+            <h1>App</h1>
+            <button onClick={() => createNewTask()}>Create task</button>
+            <ul>
+                {state.map(el => (
+                    <li key={el.id}>
+                        <p>{el.title}</p>
+                        <p>{`Complete: ${el.completed}`}</p>
+                        <button
+                            onClick={() => dispatch(completeTask(el.id))}
+                        >
+                            Completed
+                        </button>
+                        <button
+                            onClick={() => changeTitle(el.id)}
+                        >
+                            changeTitle
+                        </button>
+                        <button
+                            onClick={() => deleteTask(el.id)}
+                        >
+                            Delete
+                        </button>
+                        <hr></hr>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
+
---
+++ src/store/middleware/logger.js
@@ -0,0 +1,8 @@
+export function logger(state) {
+    return function wrapDispatch(next) {
+        return function handleAction(action) {
+            // console.log(store);
+            return next(action);
+        }
+    }
+}
